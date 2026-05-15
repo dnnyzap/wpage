a06d25f6d68ee33efa3b163798ef2f45ee8221b3
damiao <damiao.barbosa.02@gmail.com>
2026-05-15 01:36:03 -0300

    damnnnn

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,40 @@ import drawingImg from './assets/friri.jpg'
 function App() {
   return (
     <div className="main-wrapper">
+    
+      <img src={drawingImg} className='full-screen-bg' alt='' />
       
-      <div className="profile-card">
+      
+      <div className="portfolio-card">
         <header className="profile-header">
           <div className="avatar-container">
-          <img src={heroImg} alt="Danny" className="profile-avatar" />
-          <div className='discord-status.dot'></div>
+            <img src={heroImg} alt="Danny" className="profile-avatar" />
+         
+            <div className='discord-status-dot'></div>
           </div>
           <h1>Hi, I'm <span className='highlight'>Damiao</span>!</h1>
-          
-          
           <p className='status'>(=◡=)</p>
+          
           <div className='discord-presence'>
             <img
               src="https://lanyard.websandbox.xyz/api/330702585352683520"
               alt="discord status"
             />
           </div>
-
         </header>
-
 
         <section className="bio-section">
           <p>
-            I'm a <strong>25 years old</strong> student of <strong>Analysis and Systems Development</strong> based in Manaus.
-            I'm currently in the <strong>fourth period</strong> of my degree and my expertise focuses on <strong>Java, Spring Boot, and JavaScript</strong>.
+            I'm a <strong>25 years old</strong> student of <strong>Analysis and Systems Development</strong> based in Manaus. 
+            I'm in my <strong>fourth period</strong> focusing on <strong>Java, Spring Boot, and JavaScript</strong>.
           </p>
 
           <p>
-            I'm the lead of developer for <span className='highlight'>FisioNear</span>, a computer vision project designed to guide physical therapy movements using body tracking.
+            I'm the lead developer for <span className='highlight'>FisioNear</span>, using computer vision to guide physical therapy movements.
           </p>
 
           <p>
-            Outside of coding, I enjoy <strong>competitive vide games and soulslike</strong>, and creating <strong>digital art</strong>.
+            Outside of coding, I enjoy <strong>competitive video games, soulslikes</strong>, and creating <strong>digital art</strong>.
           </p>
         </section>
 
@@ -50,9 +51,6 @@ function App() {
           </div>
         </footer>
       </div>
-        
-    
-      <img src={drawingImg} className='full-screen-bg' alt='' />
     </div>
   ) 
 }
