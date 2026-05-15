31df03fdd9de04bc3a0525c023167755e9a6035a
damiao <damiao.barbosa.02@gmail.com>
2026-05-15 01:08:36 -0300

    aalgarkga

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
-/// import { GithubIcon, LinkedinIcon, MailIcon, TWitterIcon } from 'lucide-react'
-import heroImg from './assets/robinn.jpg'
 import './App.css'
+import heroImg from './assets/robinn.jpg'
+import drawingImg from './assets/friri.jpg'
 
 function App() {
   return (
     <div className="main-wrapper">
-      <div className="portfolio-card">
-        {/* Hero Image */}
-        <nav className="navbar">
-          <img src={heroImg} alt="Danny icon" className="nav-avatar" />
-
-          <div className="nav-links">
-            <a href="#work">Work</a>
-            <a href="#contact">Contact</a>
+      
+      <div className="profile-card">
+        <header className="profile-header">
+          <div className="avatar-container">
+          <img src={heroImg} alt="Danny" className="profile-avatar" />
+          <div className='discord-status.dot'></div>
           </div>
+          <h1>Hi, I'm <span className='highlight'>Damiao</span>!</h1>
           
-        </nav>
+          
+          <p className='status'>(=◡=)</p>
+          <div className='discord-presence'>
+            <img
+              src="https://lanyard.websandbox.xyz/api/330702585352683520"
+              alt="discord status"
+            />
+          </div>
 
-        <header className="hero">
-          <p className="overline">Hello, I'm</p>
-          <h1>Danny</h1>
+        </header>
 
-          <p className="description">
-            blabaerelabvalerbaelbjalerbaerlgkljeakmvaleba <strong> slbakbelkbel </strong>
+
+        <section className="bio-section">
+          <p>
+            I'm a <strong>25 years old</strong> student of <strong>Analysis and Systems Development</strong> based in Manaus.
+            I'm currently in the <strong>fourth period</strong> of my degree and my expertise focuses on <strong>Java, Spring Boot, and JavaScript</strong>.
           </p>
-   <div className='social-links'> 
+
+          <p>
+            I'm the lead of developer for <span className='highlight'>FisioNear</span>, a computer vision project designed to guide physical therapy movements using body tracking.
+          </p>
+
+          <p>
+            Outside of coding, I enjoy <strong>competitive vide games and soulslike</strong>, and creating <strong>digital art</strong>.
+          </p>
+        </section>
+
+        <footer className='card-footer'>
+          <div className='social-links'>
             <a href='https://github.com/dnnyzap' target="_blank" rel="noreferrer">Github</a>
             <a href='https://www.linkedin.com/in/damiaonunes' target='_blank' rel='noreferrer'>Linkedin</a>
             <a href='mailto:damiao.barbosa.02@gmail.com'>Email</a>
             <a href='https://x.com/dnnyzap' target='_blank' rel='noreferrer'>X</a>
           </div>
-        </header>
+        </footer>
       </div>
+        
+    
+      <img src={drawingImg} className='full-screen-bg' alt='' />
     </div>
-  )
+  ) 
 }
 
 export default App
