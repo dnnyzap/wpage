cae26471c90e8ae7456ca8c573a363ba647b54bf
damiao <damiao.barbosa.02@gmail.com>
2026-05-22 23:55:37 -0300

    add oneko script

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import defaultheroImg from './assets/robinn.jpg'
+import defaultheroImg from './assets/icon.jpg'
 import drawingImg from './assets/friri.jpg'
 
 function App() {
